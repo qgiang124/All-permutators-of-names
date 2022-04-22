02a345ed28ea6e3c0a6b6bf749b6e82026329bcd
qgiang124 <quynhgiangg124@gmail.com>
2022-04-21 23:00:28 -0500

    Initial Commit

---
+++ Lab9/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab9
+//
+//  Created by Giang Tran on 4/21/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
